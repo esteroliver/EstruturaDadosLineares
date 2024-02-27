045345b054a6f3d9d27fa154bec00cb0c516ab50
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-02-26 21:04:02 -0300

    pilha em java

---
+++ pilha/pilha.java
@@ -0,0 +1,34 @@
+public class pilha{
+    private Object array[];
+    private int top = -1;
+    public void pilha(){
+        array = (Object[]) new Object[1];
+    }
+    public int size(){
+        return top+1;
+    }
+    public boolean isEmpty(){
+        if(top == -1) return true;
+        return false;
+    }
+    public Object objectTop(){
+        if(isEmpty()) throw new ExceptionInInitializerError();
+        return array[top];
+    }
+    public Object pop(){
+        if(isEmpty()) throw new ExceptionInInitializerError();
+        top--;
+        return array[top];
+    }
+    public void push(int x){
+        if(array.length-1 == top){
+            Object[] aux;
+            aux = (Object[]) new Object[top*2];
+            for(int i = 0; i < top; i++){
+                aux[i] = array[i];
+            }
+            array = aux;
+        }
+        array[top++] = x;
+    }
+}
