45a7dfb5c0c8c0006f29f8f331756e5c86f06bdc
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2024-03-06 10:28:07 -0300

    questão da energia solar

--- questao/EnergiaSolar.java
+++ questao/EnergiaSolar.java
@@ -5,7 +5,7 @@ class EnergiaSolar{
     
     public EnergiaSolar(int tam){
         en = new int[tam];
-        linha_forca = new Pilha(tam);
+        linha_forca = new Pilha(1);
         i = 0;
     }
     public void inserir(int e){
@@ -13,31 +13,19 @@ class EnergiaSolar{
         i++;
     }
     public Pilha criarLinhaForca(Pilha percorrer){
-        Pilha linha = new Pilha(1);
         Pilha resto = new Pilha(1);
         while(!percorrer.isEmpty()){
             int n1 = percorrer.pop();
             int n2 = 0;
-            if(percorrer.size() > 1){
+            if(percorrer.size() >= 1){
                 n2 = percorrer.pop();
                 percorrer.push(n2);
             }
-            if(n1 > n2){
-                linha.push(n1);
+            if(n1 < n2){
                 resto.push(n2);
-            }
-            else{
-                linha.push(n1);
-                linha.push(n2);
             }
         }
         linha_forca = resto;
         return linha_forca;
     }   
-    public void mostrarLinhaForca(){
-        for(int i = 0; i < linha_forca.size(); i++){
-            int x = linha_forca.pop();
-            System.out.println(x + " ");
-        }
-    }
 }
--- questao/Main.java
+++ questao/Main.java
@@ -2,21 +2,31 @@ class Main{
     public static void main(String args[]){
         //{8,6,10,4,1,5,3} 
         EnergiaSolar rua = new EnergiaSolar(7);
-        rua.inserir(8);
-        rua.inserir(6);
-        rua.inserir(10);
-        rua.inserir(4);
-        rua.inserir(1);
-        rua.inserir(5);
+        // rua.inserir(8);
+        // rua.inserir(6);
+        // rua.inserir(10);
+        // rua.inserir(4);
+        // rua.inserir(1);
+        // rua.inserir(5);
+        // rua.inserir(3);
         rua.inserir(3);
+        rua.inserir(6);
+        rua.inserir(2);
+        rua.inserir(7);
+        rua.inserir(5);
 
         Pilha percorrer = new Pilha(1);
-        percorrer.push(8);
-        percorrer.push(6);
-        percorrer.push(10);
-        percorrer.push(4);
-        percorrer.push(1);
+        // percorrer.push(3);
+        // percorrer.push(5);
+        // percorrer.push(1);
+        // percorrer.push(4);
+        // percorrer.push(10);
+        // percorrer.push(6);
+        // percorrer.push(8);
         percorrer.push(5);
+        percorrer.push(7);
+        percorrer.push(2);
+        percorrer.push(6);
         percorrer.push(3);
         
         int x = 0;
@@ -24,8 +34,8 @@ class Main{
         while(!percorrer.isEmpty()){
             percorrer = rua.criarLinhaForca(percorrer);
             x++;
-            rua.mostrarLinhaForca();
         }
+
         System.out.println(x);
     }
 }
