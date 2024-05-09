33f56bba85e399a47720276a1c5367886962198e
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-05-09 09:41:10 -0300

    size, isempty, isfirst, islast

--- lista_ll/Lista.java
+++ lista_ll/Lista.java
@@ -8,18 +8,49 @@ class Lista{
         tam = 0;
     }
 
-    /**
-     * MÉTODOS PARA ACESSAR
-     * - before(n)
-     * - after(n)
-     * MÉTODOS PARA ATUALIZAR
-     * - replaceElement(n, o)
-     * - swapElements(n, q)
-     * - insertBefore(n, o)
-     * - insertAfter(n, o)
-     * - remove(n)
-     */
+/**
+ * **Métodos genéricos**
 
-     
+- size()
+- isEmpty()
+
+**Métodos de consulta**
+
+- isFirst(p)
+- isLast(p)
+
+**Métodos de acesso**
+
+- first()
+- last()
+- prev(p) - retorna o nó antes do nó p.
+- next(p) - retorna o nó depois do nó p.
+
+**Métodos de atualização**
+
+- replaceElement(p,o) - troca o valor do elemento do nó p, colocando o valor do segundo parâmetro.
+- swapElements(p,q) - troca os valores dos elementos de dois nós. O valor do elemento de p vai para q, e o valor do elemento de q vai para q.
+- insertBefore(p,o) - inserir antes do nó p um novo nó com valor o.
+- insertAfter(p,o) - inserir depois do nó p um novo nó com valor o.
+- insertFirst(p) - inserir no início da lista o nó p.
+- insertLast(p) - inserir no final da lista o nó p.
+- remove(p) - remover o nó p da lista.
+ */
+
+     public int size(){
+        return tam;
+     }
+     public boolean isEmpty(){
+        if(tam == 0) return true;
+        return false;
+     }
+     public boolean isFirst(Object o){
+        if(o == first.getElemento()) return true;
+        return false;
+     }
+     public boolean isLast(Object o){
+        if(o == last.getElemento()) return true;
+        return false;
+     }
 
 }
